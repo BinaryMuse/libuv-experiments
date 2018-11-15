2be87df6f3aee7d3c4d1ca161816c6a5f3628f4a
Michelle Tilley <michelle@michelletilley.net>
2018-11-15 11:05:01 -0800

    Convert socket handler to lambda

--- src/main.cc
+++ src/main.cc
@@ -7,21 +7,6 @@ uv_loop_t* loop;
 struct sockaddr_in addr;
 World* world;
 
-void on_new_connection(uv_stream_t* server, int status) {
-  if (status < 0) {
-    std::cerr << "New connection error: " << uv_strerror(status) << std::endl;
-    return;
-  }
-
-  uv_tcp_t* client = new uv_tcp_t;
-  uv_tcp_init(loop, client);
-  if (uv_accept(server, (uv_stream_t*) client) == 0) {
-    world->AcceptConnection(client);
-  } else {
-    uv_close((uv_handle_t*)client, NULL);
-  }
-}
-
 int main() {
   loop = uv_default_loop();
   world = new World(loop);
@@ -32,21 +17,25 @@ int main() {
   uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
 
   uv_tcp_bind(&server, (const sockaddr*)&addr, 0);
-  int r = uv_listen((uv_stream_t*)&server, 128, on_new_connection);
+  int r = uv_listen((uv_stream_t*)&server, 128, [](uv_stream_t* server, int status) {
+    if (status < 0) {
+      std::cerr << "New connection error: " << uv_strerror(status) << std::endl;
+      return;
+    }
+
+    uv_tcp_t* client = new uv_tcp_t;
+    uv_tcp_init(loop, client);
+    if (uv_accept(server, (uv_stream_t*) client) == 0) {
+      world->AcceptConnection(client);
+    } else {
+      uv_close((uv_handle_t*)client, NULL);
+    }
+  });
+
   if (r) {
     std::cerr << "Listen error " << r << std::endl;
     return 1;
   }
 
   return uv_run(loop, UV_RUN_DEFAULT);
-  // uv_idle_t idler;
-
-  // uv_idle_init(uv_default_loop(), &idler);
-  // uv_idle_start(&idler, wait_for_a_spell);
-
-  // std::cout << "Idling..." << std::endl;
-  // uv_run(uv_default_loop(), UV_RUN_DEFAULT);
-
-  // uv_loop_close(uv_default_loop());
-  // return 0;
 }
