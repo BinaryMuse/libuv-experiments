48c052df194cf9dea80c53494b41ff219d2cce71
Michelle Tilley <michelle@michelletilley.net>
2018-11-15 10:54:41 -0800

    Add tprintf

--- src/utils.h
+++ src/utils.h
@@ -1,11 +1,14 @@
-#ifndef FILE_UTILS_H__
-#define FILE_UTILS_H__
+#pragma once
 
 #include <string>
+#include <boost/format.hpp>
 #include <uv.h>
 
 void alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf);
 uv_buf_t str_to_buf(std::string str);
 std::string& rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ");
 
-#endif /* !FILE_UTILS_H__ */
+template <typename F, typename... Args>
+inline std::string tprintf(const F& format, Args&&... args);
+
+#include "tprintf.hpp"
